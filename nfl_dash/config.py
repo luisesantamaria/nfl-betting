5ab18fcc70d4eb7cca0852353fa89f7c1cfcb212
luisesantamaria <luis.santamaria.riego@gmail.com>
2025-09-14 03:27:36 -0600

    Delete nfl_dash/config.py

--- nfl_dash/config.py
+++
@@ -1,4 +0,0 @@
-SEASON_RULES = {
-    2024: {"season_start": "2024-09-05","season_end": "2025-02-12","activate_days_before": 7,"bets_open_week": 3},
-    2025: {"season_start": "2025-09-04","season_end": "2026-02-11","activate_days_before": 7,"bets_open_week": 3},
-}
